3a8be488105702055b19bc5a08bb6863dd9ebb66
Vijayanta Jain <vijayantashah@gmail.com>
2020-06-14 17:14:20 -0400

    Rewrite PrivacyCode class into 3 different classes

--- pdroid/APK.py
+++ pdroid/APK.py
@@ -1,4 +1,4 @@
-"""Module for handling privacy-related data in APK and Android"""
+"""Module for handling privacy-related methods and data in Android APK"""
 
 import json
 import os
@@ -46,7 +46,7 @@ class APK():
                 method_id = get_method_id(each_method)
                 try:
                     api_metadata = metadata[method_id]
-                    api_methods.append(PrivacyCode(each_method, is_android_api=True))
+                    api_methods.append(PrivacyAPI(each_method, api_metadata))
                 except KeyError:
                     pass
 
@@ -58,14 +58,15 @@ class APK():
 
         for each_meth in list_of_methods:
             for _, call, _ in each_meth._method_analysis_object.get_xref_from():
-                caller = PrivacyCode(call)
+                caller = PrivacyMethod(call, each_meth)
                 if caller not in callers:
                     callers[caller] = ""
         
         return [x for x in callers.keys()]
-    
+
+    # TODO - Modify this method
     def _get_picu(self):
-        """Return list of personal information collected/used (picu) by application"""
+        """Return list of personal information collected/used (picu) by all methods"""
         picu = []
 
         for each_api_method in self._api_methods:
@@ -82,17 +83,17 @@ class APK():
             ("permissions_in_xml", [str(x) for x in self.get_permissions_in_xml()]),
         ])
 
-        api_callers = []
-        for each_api_caller in self._api_callers:
-            if not each_api_caller.is_method_analysis_object_external():
-                api_callers.append(each_api_caller.export())
-        attribute_dictionary["api_callers"] = api_callers
+        # api_callers = []
+        # for each_api_caller in self._api_callers:
+        #     if not each_api_caller.is_method_analysis_object_external():
+        #         api_callers.append(each_api_caller.export())
+        # attribute_dictionary["api_callers"] = api_callers
 
-        api_caller_callers = []
-        for each_api_double_caller in self._api_caller_callers:
-            if not each_api_double_caller.is_method_analysis_object_external():
-                api_caller_callers.append(each_api_double_caller.export())
-        attribute_dictionary["api_double_callers"] = api_caller_callers
+        # api_caller_callers = []
+        # for each_api_double_caller in self._api_caller_callers:
+        #     if not each_api_double_caller.is_method_analysis_object_external():
+        #         api_caller_callers.append(each_api_double_caller.export())
+        # attribute_dictionary["api_double_callers"] = api_caller_callers
 
         return attribute_dictionary
 
@@ -104,86 +105,124 @@ class APK():
         return f"<pdroid.APK {self.__str__()}>"
         
 
-class PrivacyCode:
-    """A wrapper class for `androguard.core.analysis.MethodAnalysis`"""
+class AbstractPrivacyMethod:
 
-    def __init__(self, method_analysis_object, is_android_api=False):
+    def __init__(self, method_analysis_object):
         self._method_analysis_object = method_analysis_object
         self._class_name = self._method_analysis_object.class_name.replace("/", ".")[1:]
         self._method_name = self._method_analysis_object.method.name
         self._id = self._class_name + self._method_name
-        if is_android_api:
-            api_metadata = get_api(self._id)
-            if api_metadata is not None:
-                self.permissions_required = api_metadata['permissions_required']
-                self.personal_information_collected = api_metadata['personal_information_collected']
-                self.is_android_api = True
-                self.api_description = api_metadata['api_description']
-
-    def get_method_name(self):
-        return self._method_name
     
     def get_class_name(self):
         return self._class_name
     
+    def get_method_name(self):
+        return self._method_name
+    
     def get_id(self):
         return self._id
-
-    def get_ag_class_name(self):
-        return "L" + self.get_class_name().replace(".", "/") + ";"
     
     def is_method_analysis_object_external(self):
         return self._method_analysis_object.is_external()
+
+    def export(self):
+        attribute_dictionary = dict([
+            ("id", self.get_id()),
+            ("class_name", self.get_class_name()),
+            ("method_name", self.get_method_name())
+        ])
+        return attribute_dictionary
+
+class PrivacyAPI(AbstractPrivacyMethod):
+
+    def __init__(self, method_analysis_object, json_object):
+        super().__init__(method_analysis_object)
+        self.permissions_required = json_object['permissions_required']
+        self.personal_information_collected = json_object['personal_information_collected']
+        self.api_description = json_object['api_description']
     
+    def get_permissions_required(self):
+        return self.permissions_required
+    
+    def get_personal_information_collected(self):
+        return self.personal_information_collected
+    
+    def get_api_description(self):
+        return self.api_description
+    
+    def export(self):
+        attribute_dictionary = super().export()
+        attr_dict = dict([
+            ("personal_information_collected", self.get_personal_information_collected()),
+            ("permissions_required", self.get_permissions_required()),
+            ("api_description", self.get_api_description())
+        ])
+        attribute_dictionary.update(attr_dict)
+    
+        return attribute_dictionary
+    
+    def __str__(self):
+        return f"{self.get_id()}"
+
+    def __repr__(self):
+        return f"<pdroid.APK.PrivacyAPI {self.__str__()}"
+
+class PrivacyMethod(AbstractPrivacyMethod):
+
+    def __init__(self, caller, api):
+        super().__init__(caller)
+        self._id = self._class_name + self._method_name + self._method_analysis_object.descriptor
+        self._permissions_required = api.get_permissions_required()
+        self._personal_information_collected = api.get_personal_information_collected()
+        self._api_related_to = api
+    
+    def get_permissions_required(self):
+        return self._permissions_required
+    
+    def get_personal_information_collected(self):
+        return self._personal_information_collected
+    
+    def get_related_api(self):
+        return self._api_related_to
+
     def get_caller_methods(self):
         """Returns a list of methods that call this method"""
         callers = []
         
         for _, call, _ in self._method_analysis_object.get_xref_from():
-            caller = PrivacyCode(call)
+            caller = PrivacyMethod(call, self.get_related_api())
             if caller not in callers:
                 callers.append(caller)
 
         return callers
-    
-    def get_caller_method_src_codes(self, caller_method):
-        """Returns the caller_method's source_code"""
-
-        if not caller_method.is_method_analysis_object_external():
-            src_code = caller_method.get_source_code()
-        else:
-            src_code = "N/A"
-        
-        return src_code
 
     def get_source_code(self):
         """Returns the source code of the method. It is 
         callers responsibility to ensure that the 
         `PrivacyMethod._method_analysis_object.method` is not 
         of type `androguard.analysis.analysis.External`"""
-        self._source_code = self._method_analysis_object.get_method().get_source()
-        return self._source_code
-
-    def get_class_name(self):
-        return self._class_name
+        return self._method_analysis_object.get_method().get_source()
     
     def export(self, include_src_code=True):
-        json_obj = dict([
-                        ("id", self.__str__()),
-                        ("class_name", str(self.get_class_name())),
-                        ("method_name", str(self.get_method_name()))
-                        ])
+        attribute_dictionary = super().__export()
+        attr_dict = dict([
+            ("id", self.get_id())
+            ("personal_information_collected", self.get_personal_information_collected()),
+            ("permissions_required", self.get_permissions_required()),
+        ])
         
         if include_src_code:
             try:
-                json_obj["source_code"] = str(self.get_source_code())
+                attr_dict["source_code"] = str(self.get_source_code())
             except Exception:
-                json_obj["source_code"] = "N/A"
+                attr_dict["source_code"] = "N/A"
+            
+        attribute_dictionary.update(attr_dict)
 
-        return json_obj
+        return attribute_dictionary
 
     def __str__(self):
-        return f"{self.get_class_name()}->{self.get_method_name()};{self._method_analysis_object.descriptor}"
+        return f"{self.get_id()}"
     
     def __repr__(self):
-        return f"<pdroid.PrivacyCode {self.__str__()}>"
+        return f"<pdroid.APK.PrivacyCode {self.__str__()}>"
